90f95371c1d195ec0695ef7dca70bd186e5b9fcb
Laith Ajail <lajail@justclean.com>
2019-04-09 10:43:04 +0300

    new

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,9 +10,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  image_url:string = environment.base_url;
+  image_url: string = environment.base_url;
 	items: any= [1,1];
-  totalAmount:number=0;
+  totalAmount =0;
   // private cartitems: any = [];
   // private total: number = 0;
   constructor(public mainservice: MainService, private router: Router) {
@@ -20,36 +20,36 @@ export class CartComponent implements OnInit {
   ngOnInit() {
   }
   ngAfterContentChecked()  {
-    if (this.mainservice.cartdata != null && this.mainservice.cartdata.length > 0){
-      var sum = this.mainservice.cartdata.reduce(function(sum, elem){ 
-                  return (parseInt(sum) + ( parseInt(elem.product_price)*parseInt(elem.cartquantity)));
-         },0);
+    if (this.mainservice.cartdata != null && this.mainservice.cartdata.length > 0) {
+      const sum = this.mainservice.cartdata.reduce(function(sum, elem) {
+                  return (parseInt(sum) + ( parseInt(elem.product_price) * parseInt(elem.cartquantity)));
+         }, 0);
       this.totalAmount = sum;
     }
 }
-incrementItem(index){
+incrementItem(index) {
 	// this.items[]=50;
 	// this.items[index]++;
 	// alert(this.items[0]);
    this.mainservice.cartdata[index].cartquantity++;
-   localStorage.setItem('cart',JSON.stringify(this.mainservice.cartdata));
+   localStorage.setItem('cart', JSON.stringify(this.mainservice.cartdata));
 }
-decrementItem(index){
+decrementItem(index) {
 	// this.items[]=60;
-	if(this.mainservice.cartdata[index].cartquantity == 1) return;
+	if (this.mainservice.cartdata[index].cartquantity === 1) { return; }
   this.mainservice.cartdata[index].cartquantity--;
-  localStorage.setItem('cart',JSON.stringify(this.mainservice.cartdata));
+  localStorage.setItem('cart', JSON.stringify(this.mainservice.cartdata));
 }
-  removeFromCart(pid){
-    for(var i=0; i<this.mainservice.cartdata.length; i++){
-     if(this.mainservice.cartdata[i].id==pid){
+  removeFromCart(pid) {
+    for (let i = 0; i < this.mainservice.cartdata.length; i++) {
+     if (this.mainservice.cartdata[i].id === pid) {
         this.mainservice.cartdata.splice(i, 1);
         break;
       }
     }
-    localStorage.setItem('cart',JSON.stringify(this.mainservice.cartdata));
+    localStorage.setItem('cart', JSON.stringify(this.mainservice.cartdata));
     this.mainservice.cartdata = JSON.parse( localStorage.getItem('cart') );
-    if(this.mainservice.cartdata.length < 1){
+    if (this.mainservice.cartdata.length < 1) {
       this.router.navigate(['/shop']);
     }
      // this.router.navigate(['/externalRedirect', { externalUrl: url }], {
